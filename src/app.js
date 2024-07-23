e488f3a21a026c043d92dcccb12b01e0afcb3d78
dammar093 <dammarrana093@gmail.com>
2024-07-23 22:06:48 +0545

    fixxx

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import helmet from "helmet";
 import morgan from "morgan";
 import router from "./routers/todo.router.js";
 import cors from 'cors'
-import ApiError from "./utils/ApiError.js";
+import errorHandler from "./utils/errorHandler.js";
 
 const app = express();
 
 
 // Middleware for security headers
+app.options('*', cors()); // Allow preflight requests for all routes
+
 app.use(helmet());
 
 // Middleware for logging
@@ -19,7 +21,12 @@ app.use(express.json());
 
 // Enable CORS for all origins
 app.use(cors());
-
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store');
+    next();
+  });
+  
+app.use(errorHandler)
 // Use the router
 app.use("/api/todos", router);
 
