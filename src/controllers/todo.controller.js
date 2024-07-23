aea2b76cb5f7e6ade7ac0bd394a10a2f69636b9a
dammar093 <dammarrana093@gmail.com>
2024-07-23 21:21:30 +0545

    fix bug

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -3,83 +3,86 @@ import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
 import { TODO } from "../models/todo.model.js";
 
+// Add Todo
 const addTodo = asyncHandler(async (req, res) => {
   const { todo } = req.body;
-  if (todo.trim() === "") {
-    return new ApiError(400, "Todo is required!");
+  if (!todo || todo.trim() === "") {
+    throw new ApiError(400, "Todo is required!");
   }
-  try {
-    const response = await TODO.create({ todo });
-    return res
-      .status(200)
-      .json(new ApiResponse(200,response, "Todo created sucessfully"));
-  } catch (error) {}
+
+  const response = await TODO.create({ todo });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, response, "Todo created successfully"));
 });
+
+// Get Todos
 const getTodos = asyncHandler(async (req, res) => {
-  try {
-    const todos = await TODO.find({});
-    // console.log(todos);
-    return res.status(200).json(new ApiResponse(200, todos));
-  } catch (err) {
-    console.log("error", err);
-  }
+  const todos = await TODO.find({});
+  return res.status(200).json(new ApiResponse(200, todos));
 });
 
+// Delete Todo
 const deleteTodo = asyncHandler(async (req, res) => {
-  try {
-    const { id } = req.params;
-    //  console.log(id);
-    const data = await TODO.findByIdAndDelete(id);
+  const { id } = req.params;
+  const data = await TODO.findByIdAndDelete(id);
 
-    if (!data) {
-      return res.status(404).json(new ApiResponse(404, "Todo item not found"));
-    }
-
-    return res.status(200).json(new ApiResponse(200,data._id, "Deleted successfully"));
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json(new ApiResponse(500, "An error occurred while deleting the todo item"));
+  if (!data) {
+    throw new ApiError(404, "Todo item not found");
   }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, data._id, "Deleted successfully"));
 });
 
-
-const updateTodo = asyncHandler(async(req,res)=>{
-  try {
-    const {_id,todo}= req.body
-      const data =await  TODO.findByIdAndUpdate(_id,{
-      $set:{
-        todo
-      }
-    })
-    return res.status(200)
-    .json(
-      new ApiResponse(200,"updated sucessfully")
-    )
-  } catch (error) {
-    
+// Update Todo
+const updateTodo = asyncHandler(async (req, res) => {
+  const { _id, todo } = req.body;
+  if (!todo || todo.trim() === "") {
+    throw new ApiError(400, "Todo is required!");
   }
-})
+
+  const data = await TODO.findByIdAndUpdate(
+    _id,
+    {
+      $set: { todo },
+    },
+    { new: true }
+  );
+
+  if (!data) {
+    throw new ApiError(404, "Todo item not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, data, "Updated successfully"));
+});
+
+// Update isComplete status
 const updatedIsComplete = asyncHandler(async (req, res) => {
-  try {
-    const { isComplete,id } = req.body;
-    console.log(isComplete,id);
-    const data = await TODO.findByIdAndUpdate(id, {
-      $set: { isComplete }
-    }, { new: true });
-    
-    return res.status(200).json({
-      status: 200,
-      message: "isComplete updated successfully",
-      data
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      status: 500,
-      message: "An error occurred while updating isComplete",
-      error: error.message
-    });
+  const { isComplete, id } = req.body;
+
+  if (typeof isComplete !== "boolean") {
+    throw new ApiError(400, "isComplete must be a boolean");
   }
+
+  const data = await TODO.findByIdAndUpdate(
+    id,
+    {
+      $set: { isComplete },
+    },
+    { new: true }
+  );
+
+  if (!data) {
+    throw new ApiError(404, "Todo item not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, data, "isComplete updated successfully"));
 });
 
-export { getTodos, deleteTodo, addTodo,updateTodo,updatedIsComplete };
+export { getTodos, deleteTodo, addTodo, updateTodo, updatedIsComplete };
