62edfe2281d60dbf96a363615607cde9762b3227
dammar093 <dammarrana093@gmail.com>
2024-07-23 23:11:15 +0545

    delete

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -3,64 +3,16 @@ import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
 import { TODO } from "../models/todo.model.js";
 
-// Add Todo
-const addTodo = asyncHandler(async (req, res) => {
-  const { todo } = req.body;
-  if (!todo || todo.trim() === "") {
-    throw new ApiError(400, "Todo is required!");
-  }
-
-  const response = await TODO.create({ todo });
-  return res
-    .status(201)
-    .json(new ApiResponse(201, response, "Todo created successfully"));
-});
 
 // Get Todos
 const getTodos = asyncHandler(async (req, res) => {
-  const todos = await TODO.find({});
-  return res.status(200).json(new ApiResponse(200, todos));
-});
-
-// Delete Todo
-const deleteTodo = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const data = await TODO.findByIdAndDelete(id);
-
-  if (!data) {
-    throw new ApiError(404, "Todo item not found");
+  try {
+    const todos = await TODO.find({});
+    return res.status(200).json(new ApiResponse(200, todos));
+  } catch (error) {
+     throw new ApiError(400)
   }
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, data._id, "Deleted successfully"));
 });
 
 
-
-// Update isComplete status
-const updatedIsComplete = asyncHandler(async (req, res) => {
-  const { isComplete, id } = req.body;
-
-  if (typeof isComplete !== "boolean") {
-    throw new ApiError(400, "isComplete must be a boolean");
-  }
-
-  const data = await TODO.findByIdAndUpdate(
-    id,
-    {
-      $set: { isComplete },
-    },
-    { new: true }
-  );
-
-  if (!data) {
-    throw new ApiError(404, "Todo item not found");
-  }
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, data, "isComplete updated successfully"));
-});
-
-export { getTodos, deleteTodo, addTodo,  updatedIsComplete };
+export { getTodos};
--- src/routers/todo.router.js
+++ src/routers/todo.router.js
@@ -1,12 +1,8 @@
 import { Router } from "express"
-import { getTodos,deleteTodo,addTodo, updatedIsComplete } from "../controllers/todo.controller.js"
+import { getTodos } from "../controllers/todo.controller.js"
 
 
 const router = Router()
-
-router.route("/").post(addTodo)
 router.route("/").get(getTodos)
-router.route("/:id").delete(deleteTodo)
-// router.route("/").patch(updateTodo)
-router.route("/updateIsComplete").patch(updatedIsComplete)
+
 export default router
